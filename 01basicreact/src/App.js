2adf64138dd08f355c96170ce837deb5d5149625
sunil <sunilseervik6@gmail.com>
2025-07-20 00:06:48 +0530

    created vite and react applocation basics

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,13 @@
+import Chai from './chai'
+function App() {
+  return (
+    <>
+    <Chai/>
+    <h1>coffee aur react | learned - created basic react and vite app - read package.json - made changes in app.js and run ||
+      
+    </h1>
+    </>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,14 @@
+ import Chai from './chai'
+
+function App() {
+   
+
+  return (
+    <> //framented coz only one tag should be returned
+     <Chai/> 
+     <h1>hello </h1>
+    </>
+  )
+}
+
+export default App
